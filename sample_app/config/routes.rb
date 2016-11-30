911b341c0951098c2c72f2aa974023100f39e330
yruijie <yangbiglock@163.com>
2016-11-30 17:56:34 +0800

    Chapter 5 done.

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
 =begin
   get 'static_pages/home'
@@ -8,11 +9,14 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   get 'static_pages/contact'
+
+  get 'users/new'
 =end
 
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
